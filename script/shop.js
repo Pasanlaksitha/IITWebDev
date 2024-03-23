94478a5486a2a362a2b7af5cce115f3078f53449
NilasiJayathilake <136611590+NilasiJayathilake@users.noreply.github.com>
2024-03-23 23:14:00 +0530

    Earliest Version of the Shop . @dev-nj

---
+++ script/shop.js
@@ -0,0 +1,12 @@
+let time = 3000;
+let slideshowImages = []
+slideshowImages = document.querySelectorAll(".slideshow img")
+function showSlide(i){
+    if (i <= slideshowImages.length-1){
+        let image = document.querySelectorAll(".slideshow img:nth-child(i)")
+        document.getElementById("slideContainer").src = image.src;
+    }else {
+        document.getElementById("slideContainer").src = document.querySelectorAll(".slideshow img:nth-child(0)")
+    }
+}
+
