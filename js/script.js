2162380671e8e14bed91b0c209ca424830b201fd
Pasan Laksitha <29460924+Pasanlaksitha@users.noreply.github.com>
2024-03-27 18:04:52 +0530

    css styles added

--- js/script.js
+++ js/script.js
@@ -1,27 +1,10 @@
-class Carousel {
-    constructor(carousel) {
-        this.carousel = carousel;
-        this.buttonPrevious = carousel.querySelector('[data-carousel-button-previous]');
-        this.buttonNext = carousel.querySelector('[data-carousel-button-next]');
-        this.slidesContainer = carousel.querySelector('[data-carousel-slides-container]');
-
-        this.currentSlide = 0;
-        this.numSlides = this.slidesContainer.children.length;
-
-        this.buttonPrevious.addEventListener('click', this.handlePrevious.bind(this));
-        this.buttonNext.addEventListener('click', this.handleNext.bind(this));
-    }
-
-    handleNext() {
-        this.currentSlide = (this.currentSlide + 1) % this.numSlides;
-        this.slidesContainer.style.transform = `translateX(-${this.currentSlide * 100}%)`;
-    }
-
-    handlePrevious() {
-        this.currentSlide = (this.currentSlide - 1 + this.numSlides) % this.numSlides;
-        this.slidesContainer.style.transform = `translateX(-${this.currentSlide * 100}%)`;
-    }
-}
-
-const carousels = document.querySelectorAll('[data-carousel]');
-carousels.forEach(carousel => new Carousel(carousel));
+document.querySelectorAll('.your-element').forEach(function(element) {
+    element.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetElement = document.querySelector(this.getAttribute('href'));
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
